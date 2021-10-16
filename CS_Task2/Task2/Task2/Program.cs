14d936124840acdc127e7b320f4b42cea8197178
Adelin Chis <adelin.chis@theaccessgroup.com>
2021-10-16 12:08:43 +0300

    refactor

--- CS_Task2/Task2/Task2/Program.cs
+++ CS_Task2/Task2/Task2/Program.cs
@@ -15,32 +15,26 @@ namespace Task2
                 provider.Mode = CipherMode.ECB;
                 provider.GenerateKey();
                 provider.GenerateIV();
-                using (FileStream outputFile = new FileStream("./output.jpg", FileMode.Create))
+                using (FileStream outputFile = new FileStream("./output", FileMode.Create))
                 {
                     using (var encryptor = provider.CreateEncryptor(provider.Key, provider.IV))
                     {
-                        using (var ms = new MemoryStream())
+                        outputFile.Write(provider.IV, 0, 16);
+                        using (var cs = new CryptoStream(outputFile, encryptor, CryptoStreamMode.Write))
                         {
-                            ms.Write(provider.IV, 0, 16);
-                            using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                            using (FileStream fsInput = new FileStream(@"../../../photo.jpg", FileMode.Open))
                             {
-                                using (FileStream fsInput = new FileStream(@"C:\Users\Adelin.Chis\Documents\ComputerSecurity\CS_Task2\Task2\Task2\photo.jpg", FileMode.Open))
+                                int data;
+                                while ((data = fsInput.ReadByte()) != -1)
                                 {
-                                    int data;
-                                    while ((data = fsInput.ReadByte()) != -1)
-                                    {
-                                        cs.WriteByte((byte)data);
-                                    }
+                                    cs.WriteByte((byte)data);
                                 }
                             }
-                            outputFile.Write(ms.ToArray());
-
-
                         }
+
                     }
                 }
             }
-
         }
     }
 }
