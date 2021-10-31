a47d99d70d4590c660b241f835b8067aa80d0119
adelingabriel08 <adelingabriel08@gmail.com>
2021-10-31 21:03:16 +0200

    task 2

---
+++ CS_Week5/Task_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static Stopwatch timer = new Stopwatch();
+        static void Main(string[] args)
+        {
+            //key creation
+            Console.WriteLine("Enter the number of bits for the key:");
+            var bits = Console.ReadLine();
+
+            Generate(int.Parse(bits));
+        }
+
+
+
+        public static void Generate(int bits)
+        {
+            DSACryptoServiceProvider myrsa = new DSACryptoServiceProvider(512);
+            int size;
+            int count = 100;
+            timer.Start();
+            for (int i = 0; i < count; i++)
+            {
+                myrsa = new DSACryptoServiceProvider(1024);
+                size = myrsa.KeySize;
+            }
+            timer.Stop();
+            Console.WriteLine($"({bits})Key creation: " + (timer.ElapsedTicks / (10 * count)).ToString() + " ms");
+
+
+
+            SHA256Managed myHash = new SHA256Managed();
+            string some_text = "this is a message to be signed";
+            //sign the message
+            byte[] signature = { };
+            timer.Reset();
+            timer.Start();
+            for (int i = 0; i < count; i++)
+            {
+                signature = myrsa.SignData(Encoding.ASCII.GetBytes(some_text));
+            }
+            timer.Stop();
+            Console.WriteLine($"({bits})Signing: " + (timer.ElapsedTicks / (10 * count)).ToString() + " ms");
+
+
+
+            timer.Reset();
+            timer.Start();
+            for (int i = 0; i < count; i++)
+            {
+                myrsa.VerifyData(Encoding.ASCII.GetBytes(some_text), signature);
+            }
+            timer.Stop();
+            Console.WriteLine($"({bits})Verifying signature: " + (timer.ElapsedTicks / (10 * count)).ToString() + " ms");
+
+        }
+    }
+}
